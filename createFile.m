f70fb98d6f5b3b66620d70689fa40888528e7e8e
abhivij <abhivij.game@gmail.com>
2015-01-16 22:57:52 +0530

    create file in a format suitable for classification

---
+++ createFile.m
@@ -0,0 +1,27 @@
+data = load('rand_sub.txt');
+m = size(data);
+
+fprintf("Loaded the dataset\n");
+pause;
+
+index = 1;
+
+for i = 1 : m
+	click = data(i,1);
+	impression = data(i,2);
+
+	for i = 1 : click
+		class_data(index,:) = [index 1 data(i,[3:end])];
+		index++;
+	end
+
+	for i = 1 : impression - click
+		class_data(index,:) = [index 0 data(i,[3:end])];
+		index++;
+	end	
+end	
+
+fprintf("Created new dataset\n");
+pause;
+
+csvwrite("input_for_class.txt", class_data);
