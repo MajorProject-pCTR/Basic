321bcf3cb86f70301cdc163eb4459d1615241dbc
aswathy <aswathy1993@gmail.com>
2015-02-25 12:24:48 +0530

    python update

---
+++ Python/randomForest.py
@@ -0,0 +1,22 @@
+from sklearn.ensemble import RandomForestClassifier
+from numpy import genfromtxt, savetxt
+
+def main():
+    #create the training & test sets, skipping the header row with [1:]
+    dataset = genfromtxt(open('Data/unrolled_train.csv','r'), delimiter=',', dtype='f8')[0:]    
+    target = [x[1] for x in dataset]
+    train = [x[2:] for x in dataset]
+    testdata = genfromtxt(open('Data/train_sub.csv','r'), delimiter=',', dtype='f8')[0:]
+    test = [x[2:] for x in testdata]
+   # test = genfromtxt(open('Data/test.csv','r'), delimiter=',', dtype='f8')[1:]
+
+    #create and train the random forest
+    #multi-core CPUs can use: rf = RandomForestClassifier(n_estimators=100, n_jobs=2)
+    rf = RandomForestClassifier(n_estimators=10)
+    rf.fit(train, target)
+    predicted_probs = [x[1] for x in rf.predict_proba(test)]
+
+    savetxt('Data/submission.csv', predicted_probs, delimiter=',', fmt='%f')
+
+if __name__=="__main__":
+    main()
