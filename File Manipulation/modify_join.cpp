a72caf5534a37dd5d31cba54261788e3f0ffc980
abhivij <abhivij.game@gmail.com>
2015-03-01 23:44:56 +0530

    for neat join

---
+++ File Manipulation/modify_join.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+
+#define MAX 50
+#define ID_L 20
+
+using namespace std;
+
+int main(){
+	int click, impression, depth, position, index, desc_id_tokencnt, titid_tokencnt, purc_k_id_tokencnt, qid_tokencnt;
+	char display_url[MAX], ad_id[ID_L], advertiser_id[ID_L], qid[ID_L], purc_k_id[ID_L], titid[ID_L], desc_id[ID_L], user_id[ID_L];
+	ifstream input_file ("join.txt");
+	ofstream output_file ("improved_join.txt");
+
+	if(input_file.is_open() && output_file.is_open()){
+		while(true){
+
+			input_file >> qid >> purc_k_id >> titid >> desc_id >> index >> y >> display_url >> ad_id >> advertiser_id >> depth >> position >> user_id >> desc_id_tokencnt >> titid_tokencnt >> purc_k_id_tokencnt >> qid_tokencnt;	
+
+			if(input_file.eof())
+				break;
+				
+			output_file << y << " " << display_url << " " << ad_id << " " << advertiser_id << " " << depth << " " << position << " " << user_id << " " << desc_id << " " << desc_id_tokencnt << " " << titid << " " << titid_tokencnt << " " << purc_k_id_tokencnt << " " << qid << " " << qid_tokencnt << " " << user_id;
+
+		}
+		input_file.close();
+		output_file.close();
+	}
+	else
+		printf("Could not open file\n");
+	
+	return 0;
+}	
+
+
