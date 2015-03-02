a03d6fff37b29892e608dba893e5c21b0ce985d5
abhivij <abhivij.game@gmail.com>
2015-03-02 14:10:49 +0530

    new plots

--- R/gen_plot.R
+++ R/gen_plot.R
@@ -1,6 +1,6 @@
 gen_plot <- function() {
 
-	ad_data <- read.table("train_sub.txt")
+	ad_data <- read.table("train_improved_join_orig.txt")
 	names(ad_data) <- c("click", "impression", "ad_urlhash", "ad_id", "advertiser_id", "depth", "position", "user_id", "desc_id", "desc_id_tokencnt", "title_id", "title_id_tokencnt", "purc_key_id", "purc_key_id_tokencnt", "query_id", "query_id_tokencnt")
 
 	print("data loaded")
@@ -12,13 +12,14 @@ gen_plot <- function() {
 		if(n!="click" && n!="impression"){ 
 			var <- ad_data[[n]]
 		
-			name <- paste(n,"Vs CTR.jpeg")
+			name <- paste(n,"Vs CTR")
+			fname <- paste(name,".jpeg",sep="")
 
-			print(name)
+			print(fname)
 
-			jpeg(name)
+			jpeg(fname)
 
-			plot(var,CTR,xlab=n,col="green")
+			plot(var,CTR,xlab=n,col="green",main=name)
 
 			dev.off()
 		}
