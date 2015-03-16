320a5e1d1d108ddc3e2f4c13640aeaf0a21d2801
sravani-potturi <j.venkata.sravani@gmail.com>
2015-03-16 21:29:07 +0530

    to convert training data to vw format

--- VowpalWabbit/conversion_to_vwformat.c
+++ VowpalWabbit/conversion_to_vwformat.c
@@ -7,7 +7,7 @@ int main(){
 	float crim,zn,indus,chas,nox,rm,age,dis,tax,ptratio,b,lstat,medv;
 	FILE *fp1=fopen("housing.data","r");
 	FILE *fp2=fopen("input.data.vw","w");
-	for(i=0;i<=505;i++){
+	for(i=1;i<505;i++){
 		fscanf(fp1, " %f %f %f %f %f %f %f %f %s %f %f %f %f %f", &crim, &zn, &indus, &chas, &nox, &rm, &age, &dis, rad, &tax, &ptratio, &b, &lstat, &medv);
 		fprintf(fp2, " %f | CRIM:%f ZN:%f INDUS:%f CHAS:%f NOX:%f RM:%f AGE:%f DIS:%f RAD:%s TAX:%f PTRATIO:%f B:%f LSTAT:%f\n", medv, crim, zn, indus, chas, nox, rm, age, dis, rad, tax, ptratio, b, lstat);
 	}
