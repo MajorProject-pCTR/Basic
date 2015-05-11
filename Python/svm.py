a279183d4c2dc34026f28d07ec880ea6813bcd1f
Aswathy Ajith <aswathy1993@gmail.com>
2015-05-11 17:56:35 +0530

    File manip after id count

--- Python/svm.py
+++ Python/svm.py
@@ -3,19 +3,19 @@ from numpy import genfromtxt, savetxt
 
 def main():
     #create the training & test sets, skipping the header row with [1:]
-    dataset = genfromtxt(open('Data/unrolled.txt','r'), delimiter=',', dtype='f8')[0:]    
-    target = [x[1] for x in dataset]
-    train = [x[2:] for x in dataset]
-    testdata = genfromtxt(open('Data/rand_csv.txt','r'), delimiter=',', dtype='f8')[0:]
-    test = [x[2:] for x in testdata]
+    dataset = genfromtxt(open('unrolled.txt','r'), delimiter=' ', dtype='f8')[0:]    
+    target = [x[0] for x in dataset]
+    train = [x[1:] for x in dataset]
+    training = genfromtxt(open('train_sub.csv','r'), delimiter=',', dtype='f8')[0:]
+    #test = [x[2:] for x in testdata]
    # test = genfromtxt(open('Data/test.csv','r'), delimiter=',', dtype='f8')[1:]
 
 
     clf = svm.SVR()
     clf.fit(train, target)
-    predicted_probs = [x for x in clf.predict(test)]
+    predicted_probs = [x[1] for x in clf.predict(train)]
 
-    savetxt('Data/submission.csv', predicted_probs, delimiter=',', fmt='%f')
+    savetxt('submission.csv', predicted_probs, delimiter=' ', fmt='%f')
 
 if __name__=="__main__":
     main()
