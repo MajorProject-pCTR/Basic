687093a2447919bf7f4e9d7d2938ef4cb1e7235d
Aswathy Ajith <aswathy1993@gmail.com>
2015-05-11 19:49:33 +0530

    added ad_url_freq

--- File Manipulation/to_join_orig3.cpp
+++ File Manipulation/to_join_orig3.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 int main(){
-	int click, impression, depth, position, i, index, query_id_freq, keyword_id_freq, title_id_freq, description_id_freq, user_id_freq;
+	int click, impression, depth, position, i, index, ad_url_freq, query_id_freq, keyword_id_freq, title_id_freq, description_id_freq, user_id_freq;
 	char display_url[MAX], ad_id[ID_L], advertiser_id[ID_L], query_id[ID_L], keyword_id[ID_L], title_id[ID_L], description_id[ID_L], user_id[ID_L];
 	ifstream input_file ("input.txt");
 	ofstream output_file ("orig.txt");
@@ -18,12 +18,12 @@ int main(){
 	if(input_file.is_open() && output_file.is_open()){
 		while(true){
 
-			input_file >> click >> impression >> display_url >> ad_id >> advertiser_id >> depth >> position >> query_id >> keyword_id >> title_id >> description_id >> user_id >> query_id_freq >> keyword_id_freq >> title_id_freq >> description_id_freq >> user_id_freq;	
+			input_file >> click >> impression >> display_url >> ad_id >> advertiser_id >> depth >> position >> query_id >> keyword_id >> title_id >> description_id >> user_id >> ad_url_freq >> query_id_freq >> keyword_id_freq >> title_id_freq >> description_id_freq >> user_id_freq;	
 
 			if(input_file.eof())
 				break;
 
-			output_file << index << " " << click << " " << impression << " " << display_url << " " << ad_id << " " << advertiser_id << " " << depth << " " << position << " " << query_id << " " << keyword_id << " " << title_id << " " << description_id << " " << user_id << " " << query_id_freq << " " << keyword_id_freq << " " << title_id_freq << " " << description_id_freq << " " << user_id_freq << "\n";
+			output_file << index << " " << click << " " << impression << " " << display_url << " " << ad_id << " " << advertiser_id << " " << depth << " " << position << " " << query_id << " " << keyword_id << " " << title_id << " " << description_id << " " << user_id << " " << ad_url_freq << " " << query_id_freq << " " << keyword_id_freq << " " << title_id_freq << " " << description_id_freq << " " << user_id_freq << "\n";
 				index++;
 			
 
